72b10f3bff6b87e08acc8889879bbe7bf85fcdf3
p977ac <142778593+p977ac@users.noreply.github.com>
2024-06-21 19:12:28 +0300

    Update schemas.py

--- tinkoff/invest/schemas.py
+++ tinkoff/invest/schemas.py
@@ -290,6 +290,7 @@ class RealExchange(_grpc_helpers.Enum):
     REAL_EXCHANGE_MOEX = 1
     REAL_EXCHANGE_RTS = 2
     REAL_EXCHANGE_OTC = 3
+    REAL_EXCHANGE_DEALER = 4
 
 
 class PortfolioSubscriptionStatus(_grpc_helpers.Enum):
