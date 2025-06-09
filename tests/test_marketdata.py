8b8f64079da0f615c1a1c7a803f559aa4997021f
NemetsSY-TCS <nemetssy-tcs@users.noreply.github.com>
2025-06-09 16:05:40 +0300

    fix: confirm_margin_trade in post_stop_order

--- tests/test_marketdata.py
+++ tests/test_marketdata.py
@@ -7,7 +7,13 @@ from google.protobuf.json_format import MessageToDict
 
 from tinkoff.invest._grpc_helpers import dataclass_to_protobuff
 from tinkoff.invest.grpc import marketdata_pb2
-from tinkoff.invest.schemas import GetMySubscriptions, MarketDataRequest
+from tinkoff.invest.schemas import (
+    GetMySubscriptions,
+    MarketDataRequest,
+    SubscribeTradesRequest,
+    SubscriptionAction,
+    TradeInstrument,
+)
 from tinkoff.invest.services import MarketDataService
 
 
@@ -41,6 +47,29 @@ def test_get_order_book(market_data_service):
 def test_get_trading_status(market_data_service):
     response = market_data_service.get_trading_status(figi=mock.Mock())  # noqa: F841
     market_data_service.get_trading_status.assert_called_once()
+
+
+def test_subscribe_trades_request():
+    expected = marketdata_pb2.MarketDataRequest(
+        subscribe_trades_request=marketdata_pb2.SubscribeTradesRequest(
+            instruments=[marketdata_pb2.TradeInstrument(figi="figi")],
+            subscription_action=SubscriptionAction.SUBSCRIPTION_ACTION_SUBSCRIBE,
+            with_open_interest=True,
+        )
+    )
+
+    result = dataclass_to_protobuff(
+        MarketDataRequest(
+            subscribe_trades_request=SubscribeTradesRequest(
+                instruments=[TradeInstrument(figi="figi")],
+                subscription_action=SubscriptionAction.SUBSCRIPTION_ACTION_SUBSCRIBE,
+                with_open_interest=True,
+            )
+        ),
+        marketdata_pb2.MarketDataRequest(),
+    )
+
+    assert MessageToDict(result) == MessageToDict(expected)
 
 
 def test_market_data_request_get_my_subscriptions():
